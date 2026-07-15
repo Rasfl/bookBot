be7d049dcbb52aac4f9c059a28bfd61b892c6fa4
Rasfl <rafav.lene@gmail.com>
2026-07-15 22:13:36 +0000

    Started the code in main.py

--- runningPython/main.py
+++ runningPython/main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath: str):
+    with open(filepath) as f:
+        file_contents = f.read()
+    print(file_contents)
+    return file_contents
+
+filepath = "bookBot/runningPython/books/frankenstein.txt"
+
+def main((get_book_text(filepath)):
+    print(get_book_text(filepath))
