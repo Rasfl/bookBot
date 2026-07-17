621d2ac3dc4091cf43d52db516ceec9ef0b1f619
Rasfl <rafav.lene@gmail.com>
2026-07-17 04:50:05 +0000

    feat: V0.9, need to refactor all the code to publish, but it's all good.

--- runningPython/stats.py
+++ runningPython/stats.py
@@ -1,5 +1,7 @@
 filepath = "books/frankenstein.txt"
-total_words_dict: dict[str, int] = []
+total_words_dict: dict[str, int] = {}
+empty = 0
+# ---
 
 def get_book_text(filepath: str):
     with open(filepath) as f: 
@@ -8,26 +10,28 @@ def get_book_text(filepath: str):
 # ---
 
 def count_nums(file_contents: str) -> int:
-    total_words =  file_contents.split()
+    total_words = file_contents.split()
     return len(total_words)
 # ---
 
-def count_character(total_words):
+def count_character(total_words: list[str], empty: int) -> None:
+    total_words_dict[" "] = empty
     for words in total_words:
-        letter_words = words.lower().split()
+        letter_words = words.lower()
         for letter in letter_words:
-            total_words_dict = {letter: 0}
-            if letter in total_words_dict:
+            if letter not in total_words_dict:
+                total_words_dict[letter] = 1
+            else:
                 total_words_dict[letter] += 1
-    print(total_words_dict)
-    return 
-
+    print(total_words_dict) 
 # ---
+
 def main():
     file_contents = get_book_text(filepath)
+    empty = file_contents.count(" ")
     num_words = count_nums(file_contents)
     total_words = file_contents.split()
-    count_character(total_words)
+    count_character(total_words, empty)
     print(f"Found {num_words} total words")
 
 main()
