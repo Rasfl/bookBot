bb3077bfcadac262d36c524d0b4ae16fd44ec97f
Rasfl <rafav.lene@gmail.com>
2026-07-18 13:59:38 +0000

    Started the Report

---
+++ report/stats.py
@@ -0,0 +1,48 @@
+filepath = "books/frankenstein.txt"
+total_words_dict: dict[str, int] = {}
+empty = 0
+total_words_dict = []
+# ---
+
+def get_book_text(filepath: str):
+    with open(filepath) as f: 
+        text_book = f.read()
+        return text_book
+# ---
+
+def count_nums(file_contents: str) -> int:
+    total_words = file_contents.split()
+    return len(total_words)
+# ---
+
+def count_character(total_words: list[str], empty: int) -> None:
+    total_words_dict[" "] = empty
+    for words in total_words:
+        letter_words = words.lower()
+        for letter in letter_words:
+            if letter not in total_words_dict:
+                total_words_dict[letter] = 1
+            else:
+                total_words_dict[letter] += 1
+    return total_words_dict
+# ---
+
+def sort_on(vowel: tuple[str, int]):
+    return vowel[1]
+# ---
+
+def chars_dict_to_sorted_list(total_words_dict: dict[str, int]) -> list[tuple[str, int]]:
+    
+    return 
+# ---
+def main():
+    file_contents = get_book_text(filepath)
+    empty = file_contents.count(" ")
+    num_words = count_nums(file_contents)
+    total_words = file_contents.split()
+    count_character(total_words, empty)
+    print(f"Found {num_words} total words")
+
+main()
+
+# if you_see then refactor:
